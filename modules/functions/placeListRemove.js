06903818ba59f524e86bedb1d250a298ae2f85bc
Robun_Gid <robungidorig@gmail.com>
2024-11-17 14:42:53 +0300

    🔧 Fix commit
    * Fix search history remove in placeListRemove.js

--- modules/functions/placeListRemove.js
+++ modules/functions/placeListRemove.js
@@ -1,3 +1,3 @@
 export default function placeListRemove(event) {
-    document.querySelectorAll('.place-option').forEach(item => item.remove());
+    document.querySelectorAll('#place-option-container .place-option').forEach(item => item.remove());
 }
