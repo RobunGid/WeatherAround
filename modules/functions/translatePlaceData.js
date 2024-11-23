e2156a68cb11b27a983498d5f339275692aa0318
Robun_Gid <robungidorig@gmail.com>
2024-11-23 13:54:54 +0300

    🎈 Change translate api domain
    * Change lingva.ml/api to translate.plausibility.cloud/api

--- modules/functions/translatePlaceData.js
+++ modules/functions/translatePlaceData.js
@@ -9,7 +9,7 @@ export default async function translatePlaceData(placeData, lang) {
     lang = lang.length > 2 ? lang.substring(0, 2) : lang;
     if (lang === 'he') lang = 'iw';
     let placesDisplayNames = placeData.map(place => place.display_name + '\n').join('');
-    const response = await fetch(`https://lingva.ml/api/v1/auto/${lang}/${encodeURIComponent(placesDisplayNames)}`);
+    const response = await fetch(`https://translate.plausibility.cloud/api/v1/auto/${lang}/${encodeURIComponent(placesDisplayNames)}`);
     const dataJson = await response.json();
     let translatedPlacesDisplayNames = dataJson.translation.split(`\n`);
     let translatedPlaces = placeData.map((place, index) => {
